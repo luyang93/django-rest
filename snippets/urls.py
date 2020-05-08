666bb540af353cbcc48618d36c39910e8294b66c
luyang93 <510426762@qq.com>
2020-05-08 09:04:58 +0800

    add csrf

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-from django.urls import path
+from django.urls import path, include
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns += format_suffix_patterns([
+    path('', api_root),
+
+    path('snippets_csrf/', views.snippet_list_csrf, name='snippet_list_csrf'),
+    path('snippets_csrf/<int:pk>/', views.snippet_detail_csrf, name='snippet_detail_csrf'),
+    path('snippets_csrf/<int:pk>/highlight/', views.snippet_highlight_csrf, name='snippet_highlight_csrf'),
+    path('users_csrf/', views.user_list_csrf, name='user_list_csrf'),
+    path('users_csrf/<int:pk>/', views.user_detail_csrf, name='user_detail_csrf'),
+])
--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,130 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics, mixins, permissions, status, viewsets, renderers
+from rest_framework.decorators import api_view, action, permission_classes, renderer_classes
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'snippets_csrf': reverse('snippet_list_csrf', request=request, format=format),
+        'users_csrf': reverse('user_list_csrf', request=request, format=format),
+    })
+
+
+@csrf_exempt
+@permission_classes([permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly])
+def snippet_list_csrf(request, format=None):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True, context={'request': request})
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+@permission_classes([permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly])
+def snippet_detail_csrf(request, pk, format=None):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet, context={'request': request})
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+@renderer_classes([renderers.StaticHTMLRenderer])
+def snippet_highlight_csrf(request, pk, format=None):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == 'GET':
+        return JsonResponse(snippet.highlighted, safe=False)
+
+
+@csrf_exempt
+def user_list_csrf(request, format=None):
+    """
+    List all users, or create a new user.
+    """
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True, context={'request': request})
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def user_detail_csrf(request, pk, format=None):
+    """
+    Retrieve, update or delete a user.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except user.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user, context={'request': request})
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(user, data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
 
 
 @csrf_exempt
