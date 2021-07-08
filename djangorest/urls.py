06defbce74b31f386697298d1c648c1121ccdc98
luyang93 <510426762@qq.com>
2021-07-09 00:42:10 +0800

    Added GET, POST, PUT, PATCH and DELETE methods to our APIView

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    path('api/', include('profiles_api.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
